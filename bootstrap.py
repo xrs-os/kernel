8abbb7306d207c86347f46a71bcff0d3aab4ec46
0x5457 <0x5457@protonmail.com>
2021-10-23 17:58:26 +0800

    fix: remove objcopy and format bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -15,29 +15,26 @@ arch_target_mapping = {
 
 
 def rustup_install_target(target):
-    installed = subprocess.check_output(["rustup", "target", "list", "--installed"])
+    installed = subprocess.check_output(
+        ["rustup", "target", "list", "--installed"])
     installed_list = installed.decode("utf-8").splitlines()
     if target not in installed_list:
         run(["rustup", "target", "add", target])
 
 
 def objcopy():
-    exe = (
-        shutil.which("rust-objcopy")
-        or shutil.which("objcopy")
-        or shutil.which("llvm-objcopy")
-    )
-    if "rust-objcopy" in exe:
-        # Check if llvm-tools-preview component is installed 
-        installed = subprocess.check_output(
-            ["rustup", "component", "list", "--installed"]
-        )
-        if "llvm-tools-preview" not in installed.decode("utf-8"):
-            run(["rustup", "component", "add", "llvm-tools-preview"])
+    exe = (shutil.which("rust-objcopy") or shutil.which("llvm-objcopy"))
 
     if not exe:
         run(["cargo", "install", "cargo-binutils"])
         exe = "rust-objcopy"
+
+    if "rust-objcopy" in exe:
+        # Check if llvm-tools-preview component is installed
+        installed = subprocess.check_output(
+            ["rustup", "component", "list", "--installed"])
+        if "llvm-tools-preview" not in installed.decode("utf-8"):
+            run(["rustup", "component", "add", "llvm-tools-preview"])
 
     def inner(input, opts=[], output=None):
         cmd = [exe] + opts
@@ -53,6 +50,7 @@ def copyfile(src, dst):
     shutil.copyfile(src, dst)
     shutil.copymode(src, dst)
 
+
 def copy_to_build_dir(src, target_name, build_dir):
     Path(build_dir).mkdir(parents=True, exist_ok=True)
     suffix = Path().suffix
@@ -66,6 +64,7 @@ def copy_to_build_dir(src, target_name, build_dir):
 def cmd_build(args):
     build(args.target, args.build_dir, args.release, args.arch)
 
+
 def build(target, build_dir, release, arch):
     """Build target"""
 
@@ -77,20 +76,25 @@ def build(target, build_dir, release, arch):
     }
 
     if target not in targets:
-        fatal("no target named `{}`.\nall targets: [{}]".format(target, ', '.join(targets.keys())))
+        fatal("no target named `{}`.\nall targets: [{}]".format(
+            target, ', '.join(targets.keys())))
     target_path = join(build_dir, target)
-    
+
     if exists(target_path):
         return target_path
-    if exists(target_path+".exe"):
-        return target_path+".exe"
+    if exists(target_path + ".exe"):
+        return target_path + ".exe"
 
     return targets[target](release, arch, target, build_dir)
+
 
 def build_kernel(release, arch, target_name, build_dir):
     """Build kernel binary, returns binary file path."""
 
-    cargobuild = ["cargo", "-Zunstable-options", "-Zconfig-include", "--config", "include=\"kernel_cargo_config.toml\"", "build"]
+    cargobuild = [
+        "cargo", "-Zunstable-options", "-Zconfig-include", "--config",
+        "include=\"kernel_cargo_config.toml\"", "build"
+    ]
     if release:
         cargobuild += ["--release"]
 
@@ -100,7 +104,8 @@ def build_kernel(release, arch, target_name, build_dir):
 
     run(cargobuild, verbose=True)
 
-    kernel = "target/{}/{}/kernel".format(target, "release" if release else "debug")
+    kernel = "target/{}/{}/kernel".format(target,
+                                          "release" if release else "debug")
     kernel_bin = kernel + ".bin"
     objcopy()(kernel, ["--strip-all", "-O", "binary"], kernel_bin)
     return copy_to_build_dir(kernel_bin, target_name, build_dir)
@@ -116,9 +121,9 @@ def build_init_proc(release, arch, target_name, build_dir):
     rustup_install_target(target)
     cargobuild += ["--target", target]
     run(cargobuild, verbose=True)
-    src = "target/{}/{}/init_proc".format(target, "release" if release else "debug")
+    src = "target/{}/{}/init_proc".format(target,
+                                          "release" if release else "debug")
     return copy_to_build_dir(src, target_name, build_dir)
-
 
 
 def build_mkfs_naive(release, _arch, target_name, build_dir):
@@ -151,8 +156,10 @@ def build_initfs_img(_release, arch, target_name, build_dir):
 
 
 def cmd_qemu(args):
-    kernel_bin_path = build("kernel.bin", args.build_dir, args.release, args.arch)
-    initfs_img_path = build("initfs.img",  args.build_dir, args.release, args.arch)
+    kernel_bin_path = build("kernel.bin", args.build_dir, args.release,
+                            args.arch)
+    initfs_img_path = build("initfs.img", args.build_dir, args.release,
+                            args.arch)
     cmd = [
         "qemu-system-" + args.arch,
         "-smp",
@@ -162,7 +169,7 @@ def cmd_qemu(args):
         "--machine",
         "virt",
         "-nographic",
-        # "-display", 
+        # "-display",
         # "curses",
         # "-monitor",
         # "stdio",
@@ -187,9 +194,11 @@ def cmd_qemu(args):
 
     run(cmd)
 
+
 def cmd_clean(args):
     if exists(args.build_dir):
         shutil.rmtree(args.build_dir)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Build xrs-os")
@@ -200,29 +209,38 @@ def main():
         help="Specify architecture",
     )
     parser.add_argument("--release", action="store_true", default=False)
-    parser.add_argument(
-        "--build_dir", default="./build", help="Copy final artifacts to this directory"
-    )
+    parser.add_argument("--build_dir",
+                        default="./build",
+                        help="Copy final artifacts to this directory")
 
     subparser = parser.add_subparsers(title="subcommands")
 
     buildcmd = subparser.add_parser("build", help="Build the specified target")
-    buildcmd.add_argument("target", default="kernel.bin", help="Specified target")
+    buildcmd.add_argument("target",
+                          default="kernel.bin",
+                          help="Specified target")
     buildcmd.set_defaults(func=cmd_build)
 
-    qemucmd = subparser.add_parser("qemu", help="Build the kernel and run on qemu")
-    qemucmd.add_argument(
-        "--smp", type=int, help="Specify number of CPU cores", default=4
-    )
-    qemucmd.add_argument(
-        "--ram", "-m", dest="ram", help="Specify size of ram", default="4G"
-    )
-    qemucmd.add_argument(
-        "--gdb", action="store_true", help="Start gdbserver", default=False
-    )
+    qemucmd = subparser.add_parser("qemu",
+                                   help="Build the kernel and run on qemu")
+    qemucmd.add_argument("--smp",
+                         type=int,
+                         help="Specify number of CPU cores",
+                         default=4)
+    qemucmd.add_argument("--ram",
+                         "-m",
+                         dest="ram",
+                         help="Specify size of ram",
+                         default="4G")
+    qemucmd.add_argument("--gdb",
+                         action="store_true",
+                         help="Start gdbserver",
+                         default=False)
     qemucmd.set_defaults(func=cmd_qemu)
 
-    cleancmd = subparser.add_parser("clean", help="Remove artifacts that `bootstrap.py` has generated in the past")
+    cleancmd = subparser.add_parser(
+        "clean",
+        help="Remove artifacts that `bootstrap.py` has generated in the past")
     cleancmd.set_defaults(func=cmd_clean)
 
     args = parser.parse_args()
